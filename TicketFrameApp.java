6cbd9224fd32a061afea52e23efd5e46f70c94f9
falakheshabangu <161919888+falakheshabangu@users.noreply.github.com>
2024-08-06 16:32:06 +0200

    Add files via upload

---
+++ TicketFrameApp.java
@@ -0,0 +1,12 @@
+
+package ticketframeapp;
+
+import za.ac.tut.ui.TicketFrame;
+
+public class TicketFrameApp {
+
+    public static void main(String[] args) {
+        new TicketFrame();
+    }
+    
+}
